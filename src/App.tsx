0d9e6ef9db45e35c562c3586fa3cf66b551a136e
caiosw <caiosw@gmail.com>
2021-05-02 18:35:35 -0300

    global styles added with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1 className="title">Hello world</h1>
+      <GlobalStyle />
     </div>
   );
 }
-
-export default App;
