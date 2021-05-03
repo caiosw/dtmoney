9f381d2cb48922e23928825d4dea701722b90197
caiosw <caiosw@gmail.com>
2021-05-02 22:57:25 -0300

    dashboard

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,13 +4,11 @@ export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
     --shape: #fff;
-
     --red: #e52e4d;
     --blue: #5429cc;
-
+    --green: #33cc95;
     --blue-light: #6933ff;
-
-    --text-title: #3635f5;
+    --text-title: #363f5f;
     --text-body: #969c83;
   }
 
